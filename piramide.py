8f806f8dc17634139cd55debe5017ad3cf2d3814
DamianMartinDominguez <64601914+DamianMartinDominguez@users.noreply.github.com>
2023-05-13 20:02:09 -0300

    se agrego codigo encontrado en internet

--- piramide.py
+++ piramide.py
@@ -13,3 +13,21 @@ while restantes > piso_siguiente:
         por_fila=por_fila + altura
         utilizados= bloques - por_fila
         
+#codigo encontrado en internet
+bloques = int(input("Introduzca el número de bloques disponibles: "))
+ 
+altura = 0
+ 
+utilizados = 0
+por_fila = 1
+ 
+while True:
+    utilizados += por_fila
+    if utilizados > bloques:
+        break
+ 
+    altura += 1
+    por_fila += 1
+ 
+ 
+print("La altura de la pirámide es de: ", altura)
