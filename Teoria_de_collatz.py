ef1322f519148f6c4c83e5b8876320f1e44e7a9e
DamianMartinDominguez <64601914+DamianMartinDominguez@users.noreply.github.com>
2023-05-14 10:14:45 -0300

    Create Teoria_de_collatz.py

---
+++ Teoria_de_collatz.py
@@ -0,0 +1,11 @@
+#toma cualquier número entero que no sea negativo y que no sea cero y asígnale el nombre c0;
+#si es par, evalúa un nuevo c0 como c0 ÷ 2;
+#de lo contrario, si es impar, evalúe un nuevo  c0  como 3 × c0 + 1;
+#si c0 ≠ 1, salta al punto 2.
+#La hipótesis dice que, independientemente del valor inicial de c0, el valor siempre tiende a 1.
+
+#Escribe un programa que lea un número natural y ejecute los pasos anteriores siempre que c0 sea diferente de 1. 
+#También queremos que cuente los pasos necesarios para lograr el objetivo.
+#Tu código también debe mostrar todos los valores intermedios de c0.
+
+#Sugerencia: la parte más importante del problema es como transformar la idea de Collatz en un bucle while- esta es la clave del éxito.
